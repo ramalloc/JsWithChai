fd1f43ea5565b545af2f332816f58b785826cf42
ramalloc <roshangupta1887@gmail.com>
2023-11-08 12:45:41 +0530

    Described the steps to run Javascipt on online environment (Github)

---
+++ 03GithubJs.js
@@ -0,0 +1,12 @@
+// Here we are setting up javascript on github or online environment 
+{/*
+Step-1/ First make a repo on github 
+Step-2/ Go to Code, then go to CodeSpace -> Ctreate Codespace main . NOw we got the Codespace which is like vscode
+Step-3/ Now we have to make an nodeJs environment to execute javascript, for that. Got to Flie -> View -> Command Palette and 
+        Write -> container, click on Add Dev Container Config Files    
+Step-4/ Search for NodeJs in Container, If didn't find then click on show all definitions, then search for NodeJs and 
+        click on NodeJs with javasript. Select the version, we can select additonal tools then click on OK.
+Step-5/ Now we can make JS File and execute it. After that we should add, commit and push the changes on github then clode the tab.
+Step-6/ Now if we not using the Codepace then we have to delete the session, Go to Codespace section and delete it. 
+
+*/} 
