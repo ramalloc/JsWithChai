82c7386b92278fe922a6a1342b4b7b2d5098656b
ramalloc <95184750+ramalloc@users.noreply.github.com>
2023-11-11 17:33:14 +0000

    Done with the Comparision in JS

---
+++ 08Comparision.js
@@ -0,0 +1,26 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// --> We should avoid the below comparision methods
+
+// console.log("2" > 1); -> It returns true, because it converts "2" into 2; String to Number
+// console.log("02" > 1); -> It returns true, because it converts "02" into 2; String to Number
+
+// console.log(null > 0); -> It returns false, because when we compare {null} with comparision opertaors(>,<,>=,<=) 
+// it converts null to 0
+
+// console.log(null == 0);-> It returns false, because when we compare {null} with equal opertaor(==) 
+// it doesn't converts null to any number
+// console.log(null >= 0);
+
+// The below returns false
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// Strict equals (===), It checks strictly with the data type also
+console.log("2" === 2);
