649f93f73deaedf3f9739599f4a2c277ea358cf0
ramalloc <95184750+ramalloc@users.noreply.github.com>
2023-11-19 17:50:12 +0000

    Defined Number and type of defining numbers

---
+++ 12numsAndMaths.js
@@ -0,0 +1,7 @@
+// Interpreter detects the type of variable automatically, also detects the number type of variable
+const num = 400;
+console.log(num);
+
+// But We can explecitly define the Number type of variable
+const exp_num = new Number(400);
+console.log(exp_num);
