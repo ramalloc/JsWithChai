fc14b2acf528f687175805c3e889e317e5311f94
ramalloc <95184750+ramalloc@users.noreply.github.com>
2023-11-20 18:01:16 +0000

    Added toString method in Number

--- 12numsAndMaths.js
+++ 12numsAndMaths.js
@@ -5,3 +5,8 @@ console.log(num);
 // But We can explecitly define the Number type of variable
 const exp_num = new Number(400);
 console.log(exp_num);
+
+// We got Many Methods by defining the Number Explicitly 
+// Changing number to String and getting the length of the String
+
+console.log(exp_num.toString().length);
