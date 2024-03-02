177a8579a8419f94af208aafeccfb58b6b5ae7a8
ramalloc <95184750+ramalloc@users.noreply.github.com>
2024-03-02 18:22:50 +0000

    Learned IIFE functions implementation, limitations and their use cases

---
+++ 24IIFE.js
@@ -0,0 +1,35 @@
+// -> An IIFE (Immediately Invoked Function Expression) is a JavaScript function that runs as soon as it is defined.
+// These funcitons invoked right after its declaration
+// It is like arrow function without returning something
+
+/*
+-> We used to write these kind of functions to implement our database connection or to connect database as application started.
+-> The local scope got polluted from gloabal scope means in local scope there can be any variable which can affects by global 
+    scope by having same name or absence of value etc. So this wouldn't happen therefore we make a different scope by making
+    a IIFE. 
+*/
+
+// Normal function 
+function start(){
+    console.log("DB CONNECTED...")
+}
+start();
+
+// IFFE
+// We wrapped the normal function in a round bracket and call that function after its declaration liek this -> (func)()
+(function startDB(){
+    console.log("DB CONNECTED BY IIFE...")
+})();
+
+// We cannot declare another iife function without ending the earlier iife funtion by adding semicolon(;) in last.
+
+// We can implement the IIFE with arrow function
+(() => {
+    console.log(`DB Connected by arrow function`);
+})
+
+// But we can implement normal functions after it without adding semicolon.
+function start2(){
+    console.log("DB CONNECTED...")
+}
+start2();
